049a87f9e4e74603fdc03c4595b309f4b3cdca01
Vikas Yadav <vikashyadav62502@gmail.com>
2026-01-02 23:53:01 +0530

    random

--- server.ts
+++ server.ts
@@ -92,7 +92,7 @@ app.use('/api/sales', salesRoutes);
 app.use('/api/inventory-transactions', inventoryTransactionRoutes);
 app.use('/api/stock-returns', stockReturnRoutes);
 
-// 404 handler
+// 404 handler res
 app.use((_: Request, res: Response) => {
   res.status(404).json({ error: 'Route not found' });
 });
